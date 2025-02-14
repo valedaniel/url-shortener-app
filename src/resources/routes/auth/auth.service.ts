158a573a45589ff170ebef3a9443fd29b3d89e44
Daniel Vale <danielsvba@hotmail.com>
2025-02-14 19:44:43 -0300

    feat(auth, user): add detailed JSDoc comments for AuthService and UserService methods

--- src/resources/routes/auth/auth.service.ts
+++ src/resources/routes/auth/auth.service.ts
@@ -8,13 +8,27 @@ import User from '@app/resources/routes/user/entities/user.entity';
 import { Payload } from '@app/types/payload';
 import { InjectModel } from '@nestjs/sequelize';
 
+/**
+ * AuthService handles authentication-related operations such as login and token refresh.
+ */
 @Injectable()
 export class AuthService {
+  /**
+   * Constructs an instance of AuthService.
+   * @param tokenService - Service for handling token generation and validation.
+   * @param userRepository - Repository for accessing user data.
+   */
   constructor(
     private readonly tokenService: TokenService,
     @InjectModel(User) private readonly userRepository: typeof User,
   ) {}
 
+  /**
+   * Authenticates a user with the provided login credentials.
+   * @param login - Data transfer object containing user's email and password.
+   * @returns An object containing the authenticated user's data (excluding password), access token, and refresh token.
+   * @throws HttpException - Throws an unauthorized exception if authentication fails.
+   */
   async login(login: LoginUserDto) {
     const { email, password } = login;
 
@@ -40,6 +54,11 @@ export class AuthService {
     };
   }
 
+  /**
+   * Generates new access and refresh tokens based on the provided payload.
+   * @param payload - The payload containing user information.
+   * @returns An object containing the new access token and refresh token.
+   */
   async refreshToken(payload: Payload) {
     return this.tokenService.generateTokens(payload as User);
   }
--- src/resources/routes/user/user.service.ts
+++ src/resources/routes/user/user.service.ts
@@ -5,12 +5,27 @@ import { CreateUserDto } from '@app/resources/routes/user/dtos/create.user.dto';
 import { generateHash } from '@app/utils/generateHash/generateHash';
 import User from './entities/user.entity';
 
+/**
+ * Service responsible for handling user-related operations.
+ */
 @Injectable()
 export class UserService {
+  /**
+   * Constructs a new instance of the UserService.
+   *
+   * @param userRepository - The repository for accessing user data.
+   */
   constructor(
     @InjectModel(User) private readonly userRepository: typeof User,
   ) {}
 
+  /**
+   * Creates a new user.
+   *
+   * @param user - The data transfer object containing user details.
+   * @returns The created user.
+   * @throws {HttpException} If a user with the given email already exists.
+   */
   async create(user: CreateUserDto) {
     const userExists = await this.userRepository.findOne({
       where: { email: user.email },
