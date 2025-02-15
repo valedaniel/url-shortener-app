1b8f811261991cc803e5ebd6e21e4ab656cead27
Daniel Vale <danielsvba@hotmail.com>
2025-02-15 16:19:34 -0300

    feat(user): add documentation comments for UserController and create method

--- src/resources/routes/user/user.controller.ts
+++ src/resources/routes/user/user.controller.ts
@@ -3,10 +3,22 @@ import { UserService } from '@app/resources/routes/user/user.service';
 import { Public } from '@app/utils/decorators';
 import { Body, Controller, Post } from '@nestjs/common';
 
+/**
+ * Controller for handling user-related operations.
+ */
 @Controller('users')
 export class UserController {
+  /**
+   * Creates an instance of UserController.
+   * @param service - The user service to handle user operations.
+   */
   constructor(private readonly service: UserService) {}
 
+  /**
+   * Creates a new user.
+   * @param user - The data transfer object containing user details.
+   * @returns The created user.
+   */
   @Post()
   @Public()
   create(@Body() user: CreateUserDto) {
