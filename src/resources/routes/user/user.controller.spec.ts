de54c494b10287a318a011b426e604acaf581aff
Daniel Vale <danielsvba@hotmail.com>
2025-02-15 16:19:42 -0300

    test(user): add unit tests for UserController create method

---
+++ src/resources/routes/user/user.controller.spec.ts
@@ -0,0 +1,52 @@
+import User from '@app/resources/routes/user/entities/user.entity';
+import { Test, TestingModule } from '@nestjs/testing';
+import { CreateUserDto } from './dtos/create.user.dto';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+
+describe('UserController', () => {
+  let controller: UserController;
+  let service: UserService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UserController],
+      providers: [
+        {
+          provide: UserService,
+          useValue: {
+            create: jest.fn().mockResolvedValue({}),
+          },
+        },
+      ],
+    }).compile();
+
+    controller = module.get<UserController>(UserController);
+    service = module.get<UserService>(UserService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should call UserService.create with the correct parameters', async () => {
+      const createUserDto: CreateUserDto = {
+        email: 'test@email.com',
+        password: 'testpass',
+      };
+      await controller.create(createUserDto);
+      expect(service.create).toHaveBeenCalledWith(createUserDto);
+    });
+
+    it('should return the result of UserService.create', async () => {
+      const createUserDto: CreateUserDto = {
+        email: 'test@email.com',
+        password: 'testpass',
+      };
+      const result = { id: 1, ...createUserDto };
+      jest.spyOn(service, 'create').mockResolvedValue(result as User);
+      expect(await controller.create(createUserDto)).toBe(result);
+    });
+  });
+});
